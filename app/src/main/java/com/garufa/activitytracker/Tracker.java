aecfef0a49771a960d79bf4100a0455c576b5ad4
jes57 <jasonstephens1084@gmail.com>
2014-11-02 14:41:37 -0500

    Finished tutorial 2: Added button

--- app/src/main/java/com/garufa/activitytracker/Tracker.java
+++ app/src/main/java/com/garufa/activitytracker/Tracker.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class Tracker extends Activity {
@@ -12,6 +15,17 @@ public class Tracker extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
+
+        final TextView firstTextView = (TextView) findViewById(R.id.textView);
+
+        Button firstButton = (Button) findViewById(R.id.button);
+
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstTextView.setText("You clicked!");
+            }
+        });
     }
 
 
