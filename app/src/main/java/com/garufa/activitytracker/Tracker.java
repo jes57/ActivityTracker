172617fac7579a354b3f23039e81eaba25c48182
jes57 <jasonstephens1084@gmail.com>
2014-11-05 08:00:12 -0500

    End of tutorial 2

--- app/src/main/java/com/garufa/activitytracker/Tracker.java
+++ app/src/main/java/com/garufa/activitytracker/Tracker.java
@@ -6,26 +6,32 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Tracker extends Activity {
-
+    private Button answerYesButton, answerNoButton;
+    private EditText usersNameEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
 
-        final TextView firstTextView = (TextView) findViewById(R.id.textView);
+        answerYesButton = (Button) findViewById(R.id.answer_yes_button);
+        answerNoButton = (Button) findViewById(R.id.answer_no_button);
+        usersNameEditText = (EditText) findViewById(R.id.users_name_edit_text);
 
-        Button firstButton = (Button) findViewById(R.id.button);
-
-        firstButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                firstTextView.setText("You clicked!");
-            }
-        });
+//        final TextView firstTextView = (TextView) findViewById(R.id.textView);
+//
+//        Button firstButton = (Button) findViewById(R.id.button);
+//
+//        firstButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                firstTextView.setText("You clicked!");
+//            }
+//        });
     }
 
 
@@ -47,4 +53,23 @@ public class Tracker extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void onYesButtonClick(View view) {
+
+        String usersName = String.valueOf(usersNameEditText.getText());
+
+        String yourYesResponse = "That is great " + usersName;
+
+        Toast.makeText(this, yourYesResponse, Toast.LENGTH_SHORT).show();
+
+    }
+    public void onNoButtonClick(View view) {
+
+        String usersName = String.valueOf(usersNameEditText.getText());
+
+        String yourNoResponse = "That is awful " + usersName;
+
+        Toast.makeText(this, yourNoResponse, Toast.LENGTH_LONG).show();
+    }
+
 }
